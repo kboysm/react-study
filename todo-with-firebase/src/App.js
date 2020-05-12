a4519961eae3beefa30db822328bb0b33ae8b1ac
kboysm <lishal12@naver.com>
2020-05-12 16:12:34 +0900

    to-do-list 완성

--- todo-with-firebase/src/App.js
+++ todo-with-firebase/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import TaskAdd from './TaskAdd';
 import TaskDisplay from './TaskDisplay';
-import { firestore } from './firebase'
+import firebase from './firebase'
+import Login from './Login';
 class App extends Component {
-  state = {
-    tasks: [
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
 
-    ],
-    task: ''
+      ],
+      login: true,
+      task: ''
+    }
+    if (!firebase.auth.currentUser) {
+      this.state.login = false
+    }
   }
+
   componentDidMount() {
     const tasks = [...this.state.tasks]
-    firestore.collection('tasks').get().then(docs => {
+    firebase.firestore.collection('tasks').get().then(docs => {
       docs.forEach(doc => {
         tasks.push({ todo: doc.data().todo, id: doc.id })
       })
       this.setState({ tasks })
     })
+      .catch(e => {
+        console.log(e)
+      })
   }
   deleteHandler = (id) => {
-    firestore.collection('tasks').doc(id).delete()
+    firebase.firestore.collection('tasks').doc(id).delete()
       .then(() => {
         const tasks = this.state.tasks.filter((task) => task.id !== id)
         this.setState({ tasks });
@@ -28,7 +40,7 @@ class App extends Component {
   onClickHandler = (e) => {
     e.preventDefault(); //form은 기본적으로 추가버튼을 누르면 새로고침된다 그걸 방지하기 위해 이걸 해줌
 
-    firestore.collection('tasks').add({ todo: this.state.task })
+    firebase.firestore.collection('tasks').add({ todo: this.state.task })
       .then(r => {
         const tasks = [...this.state.tasks, { todo: this.state.task, id: r.id }];
         this.setState({
@@ -42,22 +54,35 @@ class App extends Component {
       task: e.target.value // e.target 은 이벤트가 시작된 DOM의 요소를 의미
     })
   }
+  checkLogin = () => {
+    if (firebase.auth.currentUser != null) {
+      this.setState({
+        login: true
+      })
+    }
+  }
   render() {
 
     return (
       <div className="container">
-        <TaskAdd
-          value={this.state.task}
-          changeHandler={this.onChangeHandler}
-          clickHandler={this.onClickHandler}
-        ></TaskAdd>
-        <div>
-          <h2>할일 목록</h2>
-          <TaskDisplay
-            tasks={this.state.tasks}
-            deleteHandler={this.deleteHandler}
-          ></TaskDisplay>
-        </div>
+        {this.state.login ?
+          <div>
+            <TaskAdd
+              value={this.state.task}
+              changeHandler={this.onChangeHandler}
+              clickHandler={this.onClickHandler}
+            ></TaskAdd>
+            <div>
+              <h2>할일 목록</h2>
+              <TaskDisplay
+                tasks={this.state.tasks}
+                deleteHandler={this.deleteHandler}
+              ></TaskDisplay>
+            </div>
+
+          </div>
+          : <Login login={this.checkLogin}></Login>
+        }
       </div>
     )
   }
---
+++ todo-with-firebase/src/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import firebase from './firebase'
+class Login extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    }
+    onChangeHandler = (e) => {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+    onClickHandler = e => {
+        e.preventDefault();
+        firebase.DosignInWithEmailAndPassword(this.state.username, this.state.password)
+            .then(r => {
+                console.log(r)
+                this.props.login();
+            })
+    }
+    render() {
+        const { username, password } = this.state
+        return (
+            <div>
+                <form>
+                    <input name="username" type="text" value={username} onChange={this.onChangeHandler} />
+                    <input name="password" type="password" value={password} onChange={this.onChangeHandler} />
+                    <button onClick={this.onClickHandler}>로그인</button>
+                </form>
+            </div>
+        )
+
+    }
+}
+export default Login;
