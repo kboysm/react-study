d730e828441a92d30d02ec3ed7c9843f379bd442
kboysm <lishal12@naver.com>
2020-05-19 02:11:24 +0900

    Loading

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -7,42 +7,50 @@ import Movie from './Movie'
 class App extends React.Component {
   state = {
 
-    movies: [
-      {
-        title: "행복한 세상의 족제비",
-        poster: "https://t1.daumcdn.net/tvpot/thumb/vae3auRHH4E4RkPuvRPKqKL/thumb.png?t=1430669993436"
-      },
-      {
-        title: "보노보노",
-        poster: "https://t1.daumcdn.net/liveboard/fanzeel/5f1b14c3dff24410892506ca89ae9016.JPG"
-      },
-      {
-        title: "포켓몬스터",
-        poster: "https://i.ytimg.com/vi/XZApRsbSfc8/maxresdefault.jpg"
-      },
-      {
-        title: "나루토",
-        poster: "https://i.ytimg.com/vi/N_3Eft3kaRE/maxresdefault.jpg"
-      }
-    ]
   }
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
-        greeting: ' world! '
+
+        movies: [
+          {
+            title: "행복한 세상의 족제비",
+            poster: "https://t1.daumcdn.net/tvpot/thumb/vae3auRHH4E4RkPuvRPKqKL/thumb.png?t=1430669993436"
+          },
+          {
+            title: "보노보노",
+            poster: "https://t1.daumcdn.net/liveboard/fanzeel/5f1b14c3dff24410892506ca89ae9016.JPG"
+          },
+          {
+            title: "포켓몬스터",
+            poster: "https://i.ytimg.com/vi/XZApRsbSfc8/maxresdefault.jpg"
+          },
+          {
+            title: "나루토",
+            poster: "https://i.ytimg.com/vi/N_3Eft3kaRE/maxresdefault.jpg"
+          },
+          {
+            title: "원피스",
+            poster: "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2016/07/18/16/5/adbdce04-afc8-4be2-bfe6-a1279b0946d3.JPG"
+          }
+        ]
 
       })
     }, 3000)
   }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, idx) => {
+      return <Movie title={movie.title} poster={movie.poster} key={idx} />
+    })
+    return movies
+  }
+
   render() {
     return (
       <div className="App">
-
-
-        {this.state.movies.map((movie, idx) => {
-          return <Movie title={movie.title} poster={movie.poster} key={idx} />
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     )
   }
