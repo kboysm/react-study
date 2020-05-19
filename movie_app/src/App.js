f5c1a7425bc57db674aa64c07b8cca6852876a69
kboysm <lishal12@naver.com>
2020-05-19 17:52:41 +0900

    fetch

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -10,34 +10,38 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
+    // setTimeout(() => {
+    //   this.setState({
 
-        movies: [
-          {
-            title: "행복한 세상의 족제비",
-            poster: "https://t1.daumcdn.net/tvpot/thumb/vae3auRHH4E4RkPuvRPKqKL/thumb.png?t=1430669993436"
-          },
-          {
-            title: "보노보노",
-            poster: "https://t1.daumcdn.net/liveboard/fanzeel/5f1b14c3dff24410892506ca89ae9016.JPG"
-          },
-          {
-            title: "포켓몬스터",
-            poster: "https://i.ytimg.com/vi/XZApRsbSfc8/maxresdefault.jpg"
-          },
-          {
-            title: "나루토",
-            poster: "https://i.ytimg.com/vi/N_3Eft3kaRE/maxresdefault.jpg"
-          },
-          {
-            title: "원피스",
-            poster: "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2016/07/18/16/5/adbdce04-afc8-4be2-bfe6-a1279b0946d3.JPG"
-          }
-        ]
+    //     movies: [
+    //       {
+    //         title: "행복한 세상의 족제비",
+    //         poster: "https://t1.daumcdn.net/tvpot/thumb/vae3auRHH4E4RkPuvRPKqKL/thumb.png?t=1430669993436"
+    //       },
+    //       {
+    //         title: "보노보노",
+    //         poster: "https://t1.daumcdn.net/liveboard/fanzeel/5f1b14c3dff24410892506ca89ae9016.JPG"
+    //       },
+    //       {
+    //         title: "포켓몬스터",
+    //         poster: "https://i.ytimg.com/vi/XZApRsbSfc8/maxresdefault.jpg"
+    //       },
+    //       {
+    //         title: "나루토",
+    //         poster: "https://i.ytimg.com/vi/N_3Eft3kaRE/maxresdefault.jpg"
+    //       },
+    //       {
+    //         title: "원피스",
+    //         poster: "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2016/07/18/16/5/adbdce04-afc8-4be2-bfe6-a1279b0946d3.JPG"
+    //       }
+    //     ]
 
-      })
-    }, 3000)
+    //   })
+    // }, 3000)
+    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating') //ajax로 url을 불러옴
+      .then(r => r.json())
+      .then(j => console.log(j))
+      .catch(e => console.log(e.message))
   }
 
   _renderMovies = () => {
