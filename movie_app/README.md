747d8d24def4e2f1c661e16bb5f4504067c4051d
kboysm <lishal12@naver.com>
2020-05-19 15:54:48 +0900

    Start&Dumb Component

--- movie_app/README.md
+++ movie_app/README.md
@@ -28,3 +28,62 @@ Component Lifecycle
 Render : componentWillMount() -> render() -> componentDidMount()
 
 update: componentWillReceiveProps() -> shouldComponentUpdate() ->componentWillUpdate() ->render() ->componentDidUpdate()
+
+```
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Movie.css'
+
+// class Movie extends React.Component {
+
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired,
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// class MoviePoster extends React.Component {
+
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired,
+//     }
+
+//     render() {
+//         return (
+//             <img src={this.props.poster} />
+//         )
+//     }
+// }
+         //////////// 위의 class타입의 컴포넌트와 아래 펑션타입의 컴포넌트는 서로 같다
+function Movie({ title, poster }) {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+function MoviePoster({ poster }) {
+    return (
+        <img src={poster} />
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+export default Movie
+```
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -2,34 +2,55 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Movie.css'
 
-class Movie extends React.Component {
+// class Movie extends React.Component {
 
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-    }
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired,
+//     }
 
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// class MoviePoster extends React.Component {
+
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired,
+//     }
+
+//     render() {
+//         return (
+//             <img src={this.props.poster} />
+//         )
+//     }
+// }
+
+function Movie({ title, poster }) {
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+function MoviePoster({ poster }) {
+    return (
+        <img src={poster} />
+    )
 }
 
-class MoviePoster extends React.Component {
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired,
-    }
-
-    render() {
-        return (
-            <img src={this.props.poster} />
-        )
-    }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
-
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
 export default Movie
