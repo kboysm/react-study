258018eef84733b54c2c3981fc90b2d4ff228911
kboysm <lishal12@naver.com>
2020-05-21 00:41:41 +0900

    react-webpack

---
+++ react_funcamentals/README.md
@@ -0,0 +1,3 @@
+# react_Project
+
+처음으로 create-react-app 모듈이 아닌 처음부터 끝까지 webpack설정 오류 하나하나 잡으면서 만들어본 react app
---
+++ react_funcamentals/webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js', //분기점 같은 느낌으로 여기서 부터 시작, webpack의 시작점을 의미하는거 같음
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    }, //위에서 합쳐진 파일들을 public폴더안에 bundle.js라는 파일로 저장한다는 의미
+
+    devServer: {
+        hot: true, //수정할 때마다 리로딩 여부를 결정
+        inline: true,//bundle에 같이 넣어주는거라고 함.. 잘 이해가 안감
+        host: '0.0.0.0',
+        port: 4000,
+        contentBase: __dirname + '/public/', //index파일의 위치
+    },
+
+    module: {//webpack의 핵심적인 부분이라고 함
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+
+}
