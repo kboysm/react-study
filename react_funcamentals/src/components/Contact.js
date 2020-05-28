74d69e692f243615d2a5f5186dcb3fa3ead7f70a
kboysm <lishal12@naver.com>
2020-05-28 23:11:39 +0900

    localStorage

--- react_funcamentals/src/components/Contact.js
+++ react_funcamentals/src/components/Contact.js
@@ -36,6 +36,20 @@ export default class Contact extends React.Component {
         this.handleRemove = this.handleRemove.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
     }
+    componentWillMount() {
+        const contactData = localStorage.contactData;
+
+        if (contactData) {
+            this.setState({
+                contactData: JSON.parse(contactData)
+            })
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (JSON.stringify(prevState.contactData) != JSON.stringify(this.state.contactData)) {
+            localStorage.contactData = JSON.stringify(this.state.contactData);
+        }
+    }
 
     handleCreate(contact) {
         this.setState({
